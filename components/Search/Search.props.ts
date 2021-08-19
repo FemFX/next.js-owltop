b816c6690895d8fdb6ddcde20a1847791874fe34
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-08-19 11:32:45 +0300

    Create Search.props.ts

---
+++ components/Search/Search.props.ts
@@ -0,0 +1,9 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export interface ISearchProps
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+
+}
